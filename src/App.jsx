7fc081223a568821d96fb43a8127ae54cec52e9d
sophienocode <sophieprotanki@gmail.com>
2026-02-09 09:48:43 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./Header/Header"
+import Projects from "./sections/Projects"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+        <Header />
+        <Projects  />
+      
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/constants/index.js
@@ -0,0 +1,54 @@
+
+
+
+const slides = [
+    
+
+
+  {
+    id: 1,
+    title: "d.tampe",
+    img: "/images/initial.png",
+  },
+  {
+    id: 2,
+    title: "Blimp.gr",
+    img: "/images/",
+  },
+  {
+    id: 3,
+    title: "Hawk Style Design",
+    img: "/images/",
+  },
+  {
+    id: 4,
+    title: "Lewis",
+    img: "/images/",
+  },
+
+ 
+  {
+    id: 5,
+    title: "Lewis",
+    img: "/images/",
+  },
+  {
+    id: 6,
+    title: "Sofi",
+    img: "/images/",
+  },
+
+  {
+    id: 7,
+    title: "d.tampe",
+    img: "/images/",
+  },
+];
+
+
+
+
+export {
+  slides,
+  
+};
