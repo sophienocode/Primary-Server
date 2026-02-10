00d980547101e325e09be4c3d688b0bd9ebd2327
sophienocode <sophieprotanki@gmail.com>
2026-02-10 09:23:07 -0800

    (Auth): add updates
    - More slides and new projects links

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,43 +11,44 @@ const slides = [
   },
   {
     id: 2,
+    title: "Frustrated Developer",
+    img: "/proj/frustraded-developer.png",
+    href: "https://frustrad-developer.vercel.app/",
+  },
+  { 
+   id: 3,
+    title: "Loading Tech Store",
+    img: "/proj/teachstore3d.png",
+    href: "https://loading-tech.vercel.app/",
+  },
+  {
+  id: 4,
     title: "Screen Menu",
     img: "/proj/screenmenu.png",
     href: "https://screen-menu.vercel.app",
   },
   {
-    id: 3,
+    id: 5,
     title: "Bubble Pro Loader",
     img: "/proj/Bubble-Pre-Loader.png",
     href: "https://bubble-pre-loader.vercel.app ",
   },
-  /*
-  {
-    id: 4,
-    title: "Responsive Image Layout",
-    img: "/proj/Responsive-Image.png",
-    href: "https://https://responsive-image-flex-box.vercel.app/ ",
-  },
-  
-  
-  */
 
- 
   {
-    id: 5,
+    id: 6,
     title: "Animated Background",
     img: "/proj/Animated-Background.png",
     href: "https://animated-background-five.vercel.app ",
   },
   {
-    id: 6,
+    id: 7,
     title: "Sign In Design",
     img: "/proj/signin-design.png",
      href: "https://designing-sigin.vercel.app ",
   },
 
   {
-    id: 7,
+    id: 8,
     title: "Link Hover",
     img: "/proj/linkhover.png",
      href: "https://link-hover-effect-one.vercel.app ",
